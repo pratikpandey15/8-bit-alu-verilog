4d21a9c38c0217fca91d37534597d4b7094a8f33
Pratik Pandey <pandeypratik425@gmail.com>
2026-08-01 21:17:44 +0530

    Update alu8.v

--- rtl/alu8.v
+++ rtl/alu8.v
@@ -1,1 +1,14 @@
+module alu8 (
+    input  [7:0] A,
+    input  [7:0] B,
+    input  [2:0] opcode,
 
+    output reg [7:0] Result,
+    output reg Carry,
+    output reg Zero,
+    output reg Overflow
+);
+
+    // ALU logic yahan likhenge
+
+endmodule
